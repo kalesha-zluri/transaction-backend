d470265f706ee9c12a78560fdcf1171545219712
shaik-zluri <shaik.k@zluri.com>
2025-01-11 12:58:22 +0530

    feat:  improve CSV validation with error handling and add transaction api handling

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,4 +1,4 @@
-import { Application, Request, Response} from "express";
+import { Application} from "express";
 import express from "express";
 import multer from "multer";
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,9 +1,11 @@
 import { Router} from "express";
 import {uploadTransactions} from '../controllers/uploadTransactions.controller';
 import { validateCSVUpload } from "../validators/csvFile.validator";
+import { addTransaction } from "../controllers/addTransaction.controller";
 
 const transactionRouter = Router();
 
 transactionRouter.post("/upload", validateCSVUpload, uploadTransactions);
+transactionRouter.post("/add",addTransaction);
 
 export default transactionRouter;
