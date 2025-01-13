4083a701562a505b1f89ef6b59f9ecb1af177506
shaik-zluri <shaik.k@zluri.com>
2025-01-13 12:15:08 +0530

    feat: add getTransactionList controller and database service for paginated transaction retrieval

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -3,11 +3,13 @@ import {uploadTransactions} from '../controllers/uploadTransactions.controller';
 import { validateCSVUpload } from "../validators/csvFile.validator";
 import { addTransaction } from "../controllers/addTransaction.controller";
 import { deleteTransaction } from "../controllers/deleteTransaction.controller";
+import { getTransactionList } from "../controllers/getTransactionList.controller";
 
 const transactionRouter = Router();
 
 transactionRouter.post("/upload", validateCSVUpload, uploadTransactions);
 transactionRouter.post("/add",addTransaction);
 transactionRouter.delete("/delete/:id",deleteTransaction);
+transactionRouter.get("/get",getTransactionList);
 
 export default transactionRouter;
