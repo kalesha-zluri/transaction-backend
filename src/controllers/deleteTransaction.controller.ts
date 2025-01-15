b9a10e3bb6cc7ddcb43e0c0bbedb0cb4b06ed804
shaik-zluri <shaik.k@zluri.com>
2025-01-15 23:01:46 +0530

    feat: improve transaction deletion logic by handling errors and checking for existing transactions

--- src/controllers/deleteTransaction.controller.ts
+++ src/controllers/deleteTransaction.controller.ts
@@ -8,16 +8,17 @@ export const deleteTransaction = async (
 ) => {
   try {
     const { id } = req.params;
-    const deletedTransaction = await softDeleteTransaction(parseInt(id));
-    if (!deletedTransaction) {
-      res.status(404).json({ error: "Transaction not found" });
+    const result = await softDeleteTransaction(parseInt(id));
+
+    if ("error" in result) {
+      res.status(400).json({ error: result.error });
+      return;
     }
-    res
-      .status(200)
-      .json({
-        message: "Transaction deleted successfully",
-        data: deletedTransaction,
-      });
+
+    res.status(200).json({
+      message: "Transaction deleted successfully",
+      data: result,
+    });
   } catch (error) {
     next(error);
   }
