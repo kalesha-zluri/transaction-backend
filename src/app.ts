57dead9d0917cbc3ad9601ad91a7fc238a66de0b
shaik-zluri <shaik.k@zluri.com>
2025-01-17 16:39:10 +0530

    refactor: remove error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application } from "express";
 import transactionRouter from "./routes/transaction.routes";
 import { setupMiddleware } from "./middlewares/middlewares";
-import { errorHandler } from "./middlewares/errorHandler";
 
 const app: Application = express();
 
@@ -10,8 +9,5 @@ setupMiddleware(app);
 
 // Routes
 app.use("/api/v1/transactions", transactionRouter);
-
-// Error handling middleware
-app.use(errorHandler);
 
 export default app;
